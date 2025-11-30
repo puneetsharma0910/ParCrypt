0ad323f32c66c5ffaac6258e1f3ee9016490599b
Puneet Sharma <puneetsharma0910@gmail.com>
2025-11-30 17:53:31 +0530

    Define ProcessManagement class and Task interface

--- src/app/fileHandling/IO.cpp
+++ src/app/fileHandling/IO.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 
 IO::IO(const string &file_path){
-    file_stream.open(file_path, ios::in, ios::out, ios::binary);
+    file_stream.open(file_path, ios::in | ios::out| ios::binary);
     if(!file_stream.is_open()){
         cout << "unable to read file " << file_path << endl;
     }
 }
 
 fstream IO::getFileStream(){
-    return move(file_stream)
+    return move(file_stream);
 }
